01b4b8976cdf3539d534f176dc70e9064b374dc9
paladinlp <paladinlp@163.com>
2017-12-11 15:30:49 +0800

    Merge branch 'master' of https://github.com/paladinlp/machine_learning_in_action
    # Conflicts:
    #	trees/treePlotter.py

--- bayers/bayers.py
+++ bayers/bayers.py
@@ -30,6 +30,15 @@ def setOfWord2Vec(vocabList,inputSet):
             print('the word :%s is not in my Vocabulary!' %  word)
     return returnVec
 
+def bagOfWord2VecMN(vocabList,inputSet):
+    returnVec = [0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] += 1
+        else:
+            print('the word :%s is not in my Vocabulary!' %  word)
+    return returnVec
+
 
 
 
@@ -38,8 +47,8 @@ def trainNB0(trainMatrix,trainCategory):
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
     pAbusive = sum(trainCategory)/float(numTrainDocs)
-    p0Num = zeros(numWords); p1Num = zeros(numWords)
-    p0Denom = 0.0; p1Denom = 0.0
+    p0Num = ones(numWords);  p1Num = ones(numWords)
+    p0Denom = 2.0;  p1Denom = 2.0
     for i in range(numTrainDocs):
         if trainCategory[i] == 1:
             p1Num += trainMatrix[i]  # 最后的结果是每一个词条的计数值
@@ -47,10 +56,17 @@ def trainNB0(trainMatrix,trainCategory):
         else:
             p0Num += trainMatrix[i]  # 最后的结果是每一个词条的计数值
             p0Denom += sum(trainMatrix[i])
-    p1Vect = p1Num / p1Denom
-    p0Vect = p0Num / p0Denom
+    p1Vect = log(p1Num / p1Denom)
+    p0Vect = log(p0Num / p0Denom)
     return p0Vect, p1Vect, pAbusive
 
+def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
+    p1 = sum(vec2Classify*p1Vec)+log(pClass1)
+    p0 = sum(vec2Classify*p0Vec)+log(1.0-pClass1)
+    if p1>p0:
+        return 1
+    else:
+        return 0
 
 if __name__ == '__main__':
     listOPost, listClasses = loadDataSet()
@@ -58,5 +74,20 @@ if __name__ == '__main__':
     trainMat = []
     for postinDoc in listOPost:
         trainMat.append(setOfWord2Vec(myVocabList,postinDoc))
-    p0v,p1v,pAb = trainNB0(trainMat,listClasses)
-    print(p0v,p1v,pAb)
+    p0v, p1v, pAb = trainNB0(trainMat, listClasses)
+
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = array(setOfWord2Vec(myVocabList,testEntry))
+    print (testEntry,'classified as :', classifyNB(thisDoc ,p0v, p1v, pAb))
+
+    testEntry = ['stupid', 'garbage']
+    thisDoc = array(setOfWord2Vec(myVocabList, testEntry))
+    print(testEntry, 'classified as :', classifyNB(thisDoc, p0v, p1v, pAb))
+
+
+
+
+
+
+
+
