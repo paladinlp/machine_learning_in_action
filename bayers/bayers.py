1d367f3629513776cff615487c64240dee68d293
paladinlp <paladinlp@163.com>
2017-12-12 12:09:27 +0800

    Merge branch 'master' of https://github.com/paladinlp/machine_learning_in_action
    # Conflicts:
    #	trees/treePlotter.py

--- bayers/bayers.py
+++ bayers/bayers.py
@@ -68,6 +68,51 @@ def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
     else:
         return 0
 
+
+def textParse(bigString):  # input is big string, #output is word list
+    import re
+    listOfTokens = re.split(r'\W*', bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+
+def spamTest():
+    docList = [];
+    classList = [];
+    fullText = []
+    for i in range(1, 26):
+        wordList = textParse(open('email/spam/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(open('email/ham/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList = createVocabList(docList)  # create vocabulary
+    trainingSet = list(range(50));
+
+    testSet = []  # create test set
+    for i in range(10):
+        randIndex = int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del (trainingSet[randIndex])
+    trainMat = [];
+    trainClasses = []
+    for docIndex in trainingSet:  # train the classifier (get probs) trainNB0
+        trainMat.append(bagOfWord2VecMN(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))
+    errorCount = 0
+
+    for docIndex in testSet:  # classify the remaining items
+        wordVector = bagOfWord2VecMN(vocabList, docList[docIndex])
+        if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+            errorCount += 1
+            print("classification error", docList[docIndex])
+    print('the error rate is: ', float(errorCount) / len(testSet))
+    # return vocabList,fullText
+
+
 if __name__ == '__main__':
     listOPost, listClasses = loadDataSet()
     myVocabList = createVocabList(listOPost)
@@ -84,6 +129,7 @@ if __name__ == '__main__':
     thisDoc = array(setOfWord2Vec(myVocabList, testEntry))
     print(testEntry, 'classified as :', classifyNB(thisDoc, p0v, p1v, pAb))
 
+    spamTest()
 
 
 
